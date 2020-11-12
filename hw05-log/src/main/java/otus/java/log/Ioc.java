45fed9f8c390813b5868c5cd1f66eb81a0fda776
dvalbot <dvalbot@mail.ru>
2020-11-12 17:19:29 +0300

    hw05-log: simplification of the logic

--- hw05-log/src/main/java/otus/java/log/Ioc.java
+++ hw05-log/src/main/java/otus/java/log/Ioc.java
@@ -4,6 +4,8 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Ioc {
     private Ioc() {
@@ -16,15 +18,32 @@ public class Ioc {
 
     static class DemoInvocationHandler implements InvocationHandler {
         private final Loggable instance;
+        private final List<String> loggableMethods;
 
         public DemoInvocationHandler(Loggable instance) {
             this.instance = instance;
+            this.loggableMethods = getLoggableMethods();
+        }
+
+        private List<String> getLoggableMethods() {
+            return Arrays.stream(instance.getClass().getDeclaredMethods())
+                    .filter(m -> m.isAnnotationPresent(Log.class))
+                    .map(this::toStringSignature)
+                    .collect(Collectors.toList());
+        }
+
+        private String toStringSignature(Method method) {
+            String signature = method.toString();
+            return signature.substring(signature.indexOf(method.getName()));
+        }
+
+        private boolean isLoggable(Method method) {
+            return loggableMethods.contains(toStringSignature(method));
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Exception {
-            method = instance.getClass().getMethod(method.getName(), method.getParameterTypes());
-            if (method.isAnnotationPresent(Log.class)) {
+            if (isLoggable(method)) {
                 System.out.printf("Executed method: %s; params: %s\n", method.getName(), Arrays.toString(args));
             }
             return method.invoke(instance, args);
