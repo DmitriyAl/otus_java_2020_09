502ad98d6187f370ec731ea685e1f99031e7b95f
dvalbot <dvalbot@mail.ru>
2020-11-20 17:55:52 +0300

    hw04-gc: test application

---
+++ hw04-gk/src/main/java/otus/java/gk/GcDemo.java
@@ -0,0 +1,82 @@
+package otus.java.gk;
+
+import com.sun.management.GarbageCollectionNotificationInfo;
+
+import javax.management.NotificationEmitter;
+import javax.management.NotificationListener;
+import javax.management.openmbean.CompositeData;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+public class GcDemo {
+    private final int batchSize;
+    private final int shift;
+    private final int timeout;
+    private long previousStart;
+
+    public GcDemo(int batchSize, int shift, int timeout) {
+        this.batchSize = batchSize;
+        this.shift = shift;
+        this.timeout = timeout;
+    }
+
+    public void start() throws InterruptedException {
+        displayPid();
+        switchOnMonitoring();
+        addMemoryLeak();
+    }
+
+    private void displayPid() {
+        System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
+    }
+
+    private void switchOnMonitoring() {
+        List<GarbageCollectorMXBean> gcbeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean gcbean : gcbeans) {
+            System.out.println("GC name:" + gcbean.getName());
+            NotificationEmitter emitter = (NotificationEmitter) gcbean;
+            NotificationListener listener = (notification, handback) -> {
+                if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
+                    GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
+                    String gcName = info.getGcName();
+                    String gcAction = info.getGcAction();
+                    String gcCause = info.getGcCause();
+
+                    long startTime = info.getGcInfo().getStartTime();
+                    long duration = info.getGcInfo().getDuration();
+
+                    System.out.println((startTime - previousStart) + "ms|"  + gcName + "|" + gcAction + "|" + gcCause + "(" + duration + " ms)");
+                    previousStart = startTime;
+                }
+            };
+            emitter.addNotificationListener(listener, null, null);
+        }
+    }
+
+    private void addMemoryLeak() throws InterruptedException {
+        int startIndex = 0;
+        int threshold = Integer.MAX_VALUE;
+        List<UUID> storage = new ArrayList<>();
+        boolean stop = false;
+        while (!stop) {
+            for (int i = 0; i < batchSize; i++) {
+                if (storage.size() == threshold) {
+                    stop = true;
+                    break;
+                }
+                int index = startIndex + i;
+                if (index < storage.size()) {
+                    storage.set(index, UUID.randomUUID());
+                } else {
+                    storage.add(UUID.randomUUID());
+                }
+            }
+            TimeUnit.MICROSECONDS.sleep(timeout);
+            startIndex += shift;
+        }
+    }
+}
---
+++ hw04-gk/Сonclusions.md
@@ -0,0 +1,6 @@
+Interval between GC calls | Name of the garbage collector used to perform the collection | Action performed by the garbage collector | Cause of the garbage collection
+--- | --- | --- | ---
+1217ms|G1 Young Generation|end of minor GC|G1 Evacuation Pause(70 ms)
+1303ms|G1 Young Generation|end of minor GC|G1 Evacuation Pause(76 ms)
+1347ms|G1 Young Generation|end of minor GC|G1 Evacuation Pause(81 ms)
+1253ms|G1 Young Generation|end of minor GC|G1 Evacuation Pause(60 ms)
