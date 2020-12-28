4e9982ae4148ed447b4937099579be69adfb60ca
dvalbot <dvalbot@mail.ru>
2020-12-28 20:18:03 +0300

    hw11-cache: improvements

--- hw11-cache/src/test/java/otus/java/cache/scenario/Scenarios.java
+++ hw11-cache/src/test/java/otus/java/cache/scenario/Scenarios.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -70,8 +71,8 @@ public class Scenarios extends AbstractHibernateTest {
     public void checkCacheWithPartialUpdate() {
         createTestData();
         checkIfEverythingIsCreated();
-        long nonCachedServiceTime = getUpdatedGetEverythingTime(userService, phoneService, addressService, 0);
-        long cachedServiceTime = getUpdatedGetEverythingTime(cachedUserService, cachedPhoneService, cachedAddressService, 1);
+        long nonCachedServiceTime = getUpdatedGetEverythingTime(userService, phoneService, addressService);
+        long cachedServiceTime = getUpdatedGetEverythingTime(cachedUserService, cachedPhoneService, cachedAddressService);
         checkIfResultDataCorrect("Carl", "+7999", "New street");
         displayTimes(nonCachedServiceTime, cachedServiceTime);
     }
@@ -119,14 +120,13 @@ public class Scenarios extends AbstractHibernateTest {
 
     private long getUpdatedGetEverythingTime(DbService<UserDto, User, Long> userService,
                                              DbService<PhoneDto, Phone, UUID> phoneService,
-                                             DbService<AddressDto, Address, Long> addressService,
-                                             int remainder) {
+                                             DbService<AddressDto, Address, Long> addressService) {
         long startTime = Calendar.getInstance().getTimeInMillis();
         for (int i = 0; i < 10; i++) {
             for (int j = 1; j <= 10; j++) {
                 UserDto userDto = userService.getById((long) j);
-                if (i % 2 == remainder) {
-                    updateUser(i, userDto, userService);
+                if (i % 5 == 0) {
+                    updateUser(j, userDto, userService);
                     userDto = userService.getById(userDto.getId());
                 }
                 for (PhoneDto phone : userDto.getPhones()) {
@@ -171,9 +171,13 @@ public class Scenarios extends AbstractHibernateTest {
         assertThat(phones).hasSize(20).extracting(PhoneDto::getUserId)
                 .allMatch(id -> id >= 1).allMatch(id -> id <= 20);
         assertThat(phones).hasSize(20).extracting(PhoneDto::getNumber).allMatch(p -> p.startsWith(phone));
+        assertThat(phonesInDb).extracting(PhoneDto::getId)
+                .containsAll(phones.stream().map(PhoneDto::getId).collect(Collectors.toList()));
         assertThat(addresses).hasSize(10).extracting(AddressDto::getStreet).allMatch(a -> a.startsWith(address));
         assertThat(addresses).hasSize(10).extracting(AddressDto::getUserId)
                 .allMatch(id -> id >= 1).allMatch(id -> id <= 20);
+        assertThat(addressesInDb).extracting(AddressDto::getId)
+                .containsAll(addresses.stream().map(AddressDto::getId).collect(Collectors.toList()));
         assertThat(users).hasSize(10).extracting(UserDto::getName).allMatch(n -> n.startsWith(name));
     }
 
