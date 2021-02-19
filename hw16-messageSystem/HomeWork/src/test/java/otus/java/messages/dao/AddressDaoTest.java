308fdff247ed0974acbf4fb4634db847693caf04
dvalbot <dvalbot@mail.ru>
2021-02-19 16:06:16 +0300

    hw16-messageSystem: tests were inherited from previous lessons

---
+++ hw16-messageSystem/HomeWork/src/test/java/otus/java/messages/dao/AddressDaoTest.java
@@ -0,0 +1,90 @@
+package otus.java.messages.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import otus.java.messages.base.AbstractHibernateTest;
+import otus.java.messages.model.Address;
+import otus.java.messages.model.Phone;
+import otus.java.messages.model.User;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AddressDaoTest extends AbstractHibernateTest {
+    private Dao<Address, Long> addressDao;
+
+    @BeforeEach
+    @Override
+    public void setUp() {
+        super.setUp();
+        addressDao = new AddressDao(sessionManager);
+    }
+
+    @Test
+    public void findNoAddresses() {
+        sessionManager.beginSession();
+        assertThat(addressDao.findAll()).isEmpty();
+        sessionManager.commitSession();
+    }
+
+    @Test
+    void findOneAddress() {
+        final User mark = new User("Mark");
+        final Address street = new Address("Some street", mark);
+        mark.setAddress(street);
+        mark.setPhones(Collections.singletonList(new Phone("+79519876543", mark)));
+
+        sessionManager.beginSession();
+
+        final Long streetId = addressDao.insertOrUpdate(street);
+
+        assertThat(addressDao.findAll()).hasSize(1).containsOnly(street);
+        assertThat(addressDao.findById(streetId)).isNotEmpty().get().isEqualTo(street);
+
+        sessionManager.commitSession();
+    }
+
+    @Test
+    void findTwoAddress() {
+        final User mark = new User("Mark");
+        final Address someStreet = new Address("Some street", mark);
+        mark.setAddress(someStreet);
+        mark.setPhones(Collections.singletonList(new Phone("+79519876543", mark)));
+        final User peter = new User("Peter");
+        final Address anotherStreet = new Address("Another street", peter);
+        peter.setAddress(anotherStreet);
+        peter.setPhones(Collections.singletonList(new Phone("+79811234567", peter)));
+
+        sessionManager.beginSession();
+
+        final Long someStreetId = addressDao.insertOrUpdate(someStreet);
+        final Long anotherStreetId = addressDao.insertOrUpdate(anotherStreet);
+
+        assertThat(addressDao.findAll()).hasSize(2).containsOnly(someStreet, anotherStreet);
+        assertThat(addressDao.findById(someStreetId)).isNotEmpty().get().isEqualTo(someStreet);
+        assertThat(addressDao.findById(anotherStreetId)).isNotEmpty().get().isEqualTo(anotherStreet);
+
+        sessionManager.commitSession();
+    }
+
+    @Test
+    void checkEagerInitialization() {
+        final User mark = new User("Mark");
+        final Address street = new Address("Some street", mark);
+        mark.setAddress(street);
+        mark.getPhones().add(new Phone("+79519876543", mark));
+        mark.getPhones().add(new Phone("+79811234567", mark));
+
+        sessionManager.beginSession();
+
+        final Long streetId = addressDao.insertOrUpdate(street);
+        final Optional<Address> optionalAddress = addressDao.findById(streetId);
+
+        sessionManager.commitSession();
+
+        assertThat(optionalAddress).isNotEmpty();
+        assertThat(optionalAddress.get().getUser().getName()).isEqualTo(mark.getName());
+    }
+}
---
+++ hw16-messageSystem/HomeWork/src/test/java/otus/java/messages/dao/UserDaoTest.java
@@ -0,0 +1,135 @@
+package otus.java.messages.dao;
+
+import org.hibernate.LazyInitializationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import otus.java.messages.base.AbstractHibernateTest;
+import otus.java.messages.model.Address;
+import otus.java.messages.model.Phone;
+import otus.java.messages.model.User;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class UserDaoTest extends AbstractHibernateTest {
+    private Dao<User, Long> userDao;
+
+    @BeforeEach
+    @Override
+    public void setUp() {
+        super.setUp();
+        userDao = new UserDao(sessionManager);
+    }
+
+    @Test
+    public void findNoUsers() {
+        sessionManager.beginSession();
+        assertThat(userDao.findAll()).isEmpty();
+        sessionManager.commitSession();
+    }
+
+    @Test
+    void findOneUser() {
+        final User mark = new User("Mark");
+        mark.setAddress(new Address("Some street", mark));
+        mark.setPhones(Collections.singletonList(new Phone("+79519876543", mark)));
+
+        sessionManager.beginSession();
+
+        final Long markId = userDao.insertOrUpdate(mark);
+
+        assertThat(userDao.findAll()).hasSize(1).containsOnly(mark);
+        assertThat(userDao.findById(markId)).isNotEmpty().get().isEqualTo(mark);
+
+        sessionManager.commitSession();
+    }
+
+    @Test
+    void findTwoUser() {
+        final User mark = new User("Mark");
+        mark.setAddress(new Address("Some street", mark));
+        mark.setPhones(Collections.singletonList(new Phone("+79519876543", mark)));
+        final User peter = new User("Peter");
+        peter.setAddress(new Address("Another street", peter));
+        peter.setPhones(Collections.singletonList(new Phone("+79811234567", peter)));
+
+        sessionManager.beginSession();
+
+        final Long markId = userDao.insertOrUpdate(mark);
+        final Long peterId = userDao.insertOrUpdate(peter);
+
+        assertThat(userDao.findAll()).hasSize(2).containsOnly(mark, peter);
+        assertThat(userDao.findById(markId)).isNotEmpty().get().isEqualTo(mark);
+        assertThat(userDao.findById(peterId)).isNotEmpty().get().isEqualTo(peter);
+
+        sessionManager.commitSession();
+    }
+
+    @Test
+    void checkLazyInitializationExceptionOutOfTransaction() {
+        final User mark = new User("Mark");
+        mark.setAddress(new Address("Some street", mark));
+        mark.getPhones().add(new Phone("+79519876543", mark));
+        mark.getPhones().add(new Phone("+79811234567", mark));
+
+        sessionManager.beginSession();
+
+        final Long markId = userDao.insertOrUpdate(mark);
+        sessionManager.commitSession();
+        sessionManager.beginSession();
+        final Optional<User> optionalUser = userDao.findById(markId);
+
+        sessionManager.commitSession();
+
+        assertThat(optionalUser).isNotEmpty();
+        assertThatThrownBy(() -> optionalUser.get().getAddress().getStreet()).isInstanceOf(LazyInitializationException.class);
+        assertThatThrownBy(() -> optionalUser.get().getPhones().size()).isInstanceOf(LazyInitializationException.class);
+    }
+
+    @Test
+    void checkLazyInitializationExceptionInsideAnotherTransaction() {
+        final User mark = new User("Mark");
+        mark.setAddress(new Address("Some street", mark));
+        final Phone phone1 = new Phone("+79519876543", mark);
+        mark.getPhones().add(phone1);
+        final Phone phone2 = new Phone("+79811234567", mark);
+        mark.getPhones().add(phone2);
+
+        sessionManager.beginSession();
+
+        final Long markId = userDao.insertOrUpdate(mark);
+        sessionManager.commitSession();
+        sessionManager.beginSession();
+        final Optional<User> optionalUser = userDao.findById(markId);
+        sessionManager.commitSession();
+        sessionManager.beginSession();
+
+        assertThat(optionalUser).isNotEmpty();
+        assertThatThrownBy(() -> optionalUser.get().getAddress().getStreet()).isInstanceOf(LazyInitializationException.class);
+        assertThatThrownBy(() -> optionalUser.get().getPhones().size()).isInstanceOf(LazyInitializationException.class);
+        sessionManager.commitSession();
+    }
+
+    @Test
+    void checkLazyInitialization() {
+        final User mark = new User("Mark");
+        mark.setAddress(new Address("Some street", mark));
+        final Phone phone1 = new Phone("+79519876543", mark);
+        mark.getPhones().add(phone1);
+        final Phone phone2 = new Phone("+79811234567", mark);
+        mark.getPhones().add(phone2);
+
+        sessionManager.beginSession();
+
+        final Long markId = userDao.insertOrUpdate(mark);
+        final Optional<User> optionalUser = userDao.findById(markId);
+
+        assertThat(optionalUser).isNotEmpty();
+        assertThat(optionalUser.get().getAddress().getStreet()).isEqualTo(mark.getAddress().getStreet());
+        assertThat(optionalUser.get().getPhones()).hasSize(2).containsOnly(phone1, phone2);
+        sessionManager.commitSession();
+    }
+}
